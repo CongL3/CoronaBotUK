1af6fc7872b5034eb606247a19455aa28aa5c1c9
Cong Le <congle11@gmail.com>
2020-04-16 18:43:53 +0700

    Adds new formatting

--- CoronaUKBot.py
+++ CoronaUKBot.py
@@ -5,7 +5,9 @@ import json
 import requests
 import pandas as pd
 import discord
+import math
 
+from bs4 import BeautifulSoup
 from discord.ext.commands import Bot
 
 BOT_PREFIX = ("?", "!")
@@ -20,27 +22,6 @@ wom_url = 'https://www.worldometers.info/coronavirus/'
 
 client = Bot(command_prefix=BOT_PREFIX)
 
-# @client.command(name='8ball',
-# 	description="test description.",
-# 	brief="test brief.",
-# 	aliases=['eightball', 'eightball2', 'eightball3'],
-# 	pass_context=True)
-# async def eight_ball(context):
-# 	possible_responses = [
-# 	'That is a resounding no1',
-# 	'That is a resounding no2',
-# 	'That is a resounding no3',
-# 	'That is a resounding no4',
-# 	'That is a resounding no5',
-# 	'That is a resounding no6'
-# 	]
-# 	await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
-
-# @client.command()
-# async def square(number):
-# 	squared_value = int(number) * int(number)
-# 	await client.say(str(number) + " squared is " + str(squared_value))
-
 def formatWith0DP(value):
 	return "{:.0f}".format(value)
 
@@ -48,7 +29,9 @@ def totalDeathByCounty(df, country):
 	return df[df['Country,Other'].str.match(country, na=False)].values[0][3]
 
 @client.command(
-		aliases=['uk', 'update', 'data']
+		aliases=['uk', 'update', 'data'],
+		description="Shows the current status of corona in the UK.",
+		brief="Corona stats for the UK"
 )
 async def deathcount():
 	r = requests.get(wom_url, headers=header)
@@ -61,9 +44,22 @@ async def deathcount():
 		)
 
 	total_cases= df_loc['TotalCases'].values[0]
-	new_cases= df_loc['NewCases'].values[0]
+	
+	if math.isnan(df_loc['NewCases'].values[0]):
+		new_cases= '...' 
+	else:
+		new_cases= df_loc['NewCases'].values[0]
+	
 	total_deaths= df_loc['TotalDeaths'].values[0]
-	new_deaths= df_loc['NewDeaths'].values[0]
+	
+	if math.isnan(df_loc['NewDeaths'].values[0]):
+		new_deaths= '...'
+	else:
+		new_deaths= df_loc['NewDeaths'].values[0]
+
+
+	soup = BeautifulSoup(r.content, 'html.parser')
+	last_updated = soup.findAll('div',attrs={"style" : "font-size:13px; color:#999; margin-top:5px; text-align:center"})[0].text
 
 	embed.set_author(name= 'CoronaUK', icon_url= 'https://cdn.discordapp.com/app-icons/700076177011900438/4a19422eb9880e8778723e0823d34416.png')
 	embed.set_thumbnail(url= 'https://cdn.discordapp.com/app-icons/700076177011900438/4a19422eb9880e8778723e0823d34416.png')
@@ -73,12 +69,14 @@ async def deathcount():
 	embed.add_field(name= 'Total Deaths', value=formatWith0DP(total_deaths), inline=True)
 	embed.add_field(name= 'New Deaths', value=str(new_deaths), inline=True)
 	embed.add_field(name= 'Mortality Rate', value="{:.2f}%".format((total_deaths/total_cases)*100), inline=True)
-	embed.set_footer(text= 'Data from Worldometers')
+	embed.set_footer(text= last_updated + ' (Worldometers)')
 
 	await client.say(embed=embed)
 
 @client.command(
-		aliases=['v']
+		aliases=['v'],
+		description="Wager for highest Corona death count by the end of 2020\nCong: Brazil, India, Italy\nOli: Brazil, China, Turkey\nMouse: China, Italy, Spain",
+		brief="Victory"
 )
 async def victory():
 	r = requests.get(wom_url, headers=header)
@@ -99,6 +97,9 @@ async def victory():
 	my_list = [(cong_total, 'Cong'), (mouse_total, 'Mouse'), (oli_total, 'Oli')]
 	my_list.sort(reverse= True)
 
+	soup = BeautifulSoup(r.content, 'html.parser')
+	last_updated = soup.findAll('div',attrs={"style" : "font-size:13px; color:#999; margin-top:5px; text-align:center"})[0].text
+
 	embed = discord.Embed(
 			colour= discord.Colour.blue()
 		)
@@ -108,7 +109,7 @@ async def victory():
 	embed.add_field(name= '1st _ _ _ _ _ _ _ _ _ _ _ _', value= my_list[0][1] + '\n' + formatWith0DP(my_list[0][0]), inline=True)
 	embed.add_field(name= '2nd _ _ _ _ _ _ _ _ _ _', value= my_list[1][1] + '\n' + formatWith0DP(my_list[1][0]), inline=True)
 	embed.add_field(name= 'Shitter', value= my_list[2][1] + '\n' + formatWith0DP(my_list[2][0]), inline=True)
-	embed.set_footer(text= 'Data from Worldometers')
+	embed.set_footer(text= last_updated + ' (Worldometers)')
 
 	await client.say(embed=embed)
 
