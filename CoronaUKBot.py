c7659d64dcf8ed3f9ba43c13da5137e4a3afdbfc
CongL3 <congle.11@gmail.com>
2020-04-16 20:52:18 +0700

    adds specific countries

--- CoronaUKBot.py
+++ CoronaUKBot.py
@@ -34,44 +34,72 @@ def totalDeathByCounty(df, country):
 		brief="Corona stats for the UK"
 )
 async def deathcount():
+	await client.say(embed=embedFromCountry('UK'))
+
+@client.command(
+		aliases=['c',],
+		description="Shows the current status of corona in the UK.",
+		brief="Corona stats for the UK",
+		pass_context=True
+)
+async def deathcountWithArg(ctx, arg):
+
+	await client.say(embed=embedFromCountry(arg))
+
+
+def isNaN(string):
+    return string != string
+
+def embedFromCountry(country):
 	r = requests.get(wom_url, headers=header)
 	df = pd.read_html(r.text)
 	df = df[0]  # why?
-	df_loc = df[df['Country,Other'].str.match('UK', na=False)]
+	df_loc = df[df['Country,Other'].str.lower().str.match(country, na=False)]
 
-	embed = discord.Embed(
-			colour= discord.Colour.blue()
+	if df_loc.size == 0 : 
+		embed = discord.Embed(
+			colour= discord.Colour.red()
 		)
 
-	total_cases= df_loc['TotalCases'].values[0]
-	
-	if math.isnan(df_loc['NewCases'].values[0]):
-		new_cases= '...' 
-	else:
-		new_cases= df_loc['NewCases'].values[0]
-	
-	total_deaths= df_loc['TotalDeaths'].values[0]
-	
-	if math.isnan(df_loc['NewDeaths'].values[0]):
-		new_deaths= '...'
-	else:
-		new_deaths= df_loc['NewDeaths'].values[0]
+		embed.set_author(name= 'CoronaUK', icon_url= 'https://cdn.discordapp.com/app-icons/700076177011900438/4a19422eb9880e8778723e0823d34416.png')
+		embed.set_thumbnail(url= 'https://cdn.discordapp.com/app-icons/700076177011900438/4a19422eb9880e8778723e0823d34416.png')
+		embed.add_field(name= 'Nigga can you spell?', value=country + ' was not found', inline=True)
+		return embed
+	else :
+
+		embed = discord.Embed(
+				colour= discord.Colour.blue()
+			)
+
+		total_cases= df_loc['TotalCases'].values[0]
+		
+		if isNaN(df_loc['NewCases'].values[0]):
+			new_cases= '...' 
+		else:
+			new_cases= df_loc['NewCases'].values[0]
+		
+		total_deaths= df_loc['TotalDeaths'].values[0]
+		
+		if isNaN(df_loc['NewDeaths'].values[0]):
+			new_deaths= '...'
+		else:
+			new_deaths= df_loc['NewDeaths'].values[0]
 
 
-	soup = BeautifulSoup(r.content, 'html.parser')
-	last_updated = soup.findAll('div',attrs={"style" : "font-size:13px; color:#999; margin-top:5px; text-align:center"})[0].text
+		soup = BeautifulSoup(r.content, 'html.parser')
+		last_updated = soup.findAll('div',attrs={"style" : "font-size:13px; color:#999; margin-top:5px; text-align:center"})[0].text
 
-	embed.set_author(name= 'CoronaUK', icon_url= 'https://cdn.discordapp.com/app-icons/700076177011900438/4a19422eb9880e8778723e0823d34416.png')
-	embed.set_thumbnail(url= 'https://cdn.discordapp.com/app-icons/700076177011900438/4a19422eb9880e8778723e0823d34416.png')
-	embed.add_field(name= 'Total Cases', value=formatWith0DP(total_cases), inline=True)
-	embed.add_field(name= 'New Cases', value=str(new_cases), inline=True)
-	embed.add_field(name= '_ _', value='_ _', inline=True)
-	embed.add_field(name= 'Total Deaths', value=formatWith0DP(total_deaths), inline=True)
-	embed.add_field(name= 'New Deaths', value=str(new_deaths), inline=True)
-	embed.add_field(name= 'Mortality Rate', value="{:.2f}%".format((total_deaths/total_cases)*100), inline=True)
-	embed.set_footer(text= last_updated + ' (Worldometers)')
+		embed.set_author(name= 'CoronaUK | Stats for ' + country, icon_url= 'https://cdn.discordapp.com/app-icons/700076177011900438/4a19422eb9880e8778723e0823d34416.png')
+		embed.set_thumbnail(url= 'https://cdn.discordapp.com/app-icons/700076177011900438/4a19422eb9880e8778723e0823d34416.png')
+		embed.add_field(name= 'Total Cases', value=formatWith0DP(total_cases), inline=True)
+		embed.add_field(name= 'New Cases', value=str(new_cases), inline=True)
+		embed.add_field(name= '_ _', value='_ _', inline=True)
+		embed.add_field(name= 'Total Deaths', value=formatWith0DP(total_deaths), inline=True)
+		embed.add_field(name= 'New Deaths', value=str(new_deaths), inline=True)
+		embed.add_field(name= 'Mortality Rate', value="{:.2f}%".format((total_deaths/total_cases)*100), inline=True)
+		embed.set_footer(text= last_updated + ' (Worldometers)')
 
-	await client.say(embed=embed)
+		return embed
 
 @client.command(
 		aliases=['v'],
